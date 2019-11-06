e771fec316832e894adfcdf034d6930dc019666f
KrystianKaminski <krystiankaminski07@gmail.com>
2019-11-06 16:09:28 +0100

    Create initial state for createExercise component

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 
 export default class CreateExercises extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: "",
+      description: "",
+      duration: 0,
+      date: new Date(),
+      users: []
+    };
+  }
   render() {
     return (
       <div>
