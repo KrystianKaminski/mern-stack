32272ace57d6d459c8a10f29bfd53a1f8cf72d60
KrystianKaminski <krystiankaminski07@gmail.com>
2019-11-06 16:16:32 +0100

    Create handlers for each future form elements

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -12,6 +12,46 @@ export default class CreateExercises extends Component {
       users: []
     };
   }
+
+  onChangeUserName(e) {
+    this.setState({
+      username: e.target.value
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value
+    });
+  }
+
+  onChangeDuration(e) {
+    this.setState({
+      duration: e.target.value
+    });
+  }
+
+  onChangeDate(date) {
+    this.setState({
+      date: date
+    });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const exercise = {
+      username: this.state.username,
+      description: this.state.description,
+      duration: this.state.duration,
+      date: this.state.date
+    };
+
+    console.log(exercise);
+
+    window.location = "/";
+  }
+
   render() {
     return (
       <div>
